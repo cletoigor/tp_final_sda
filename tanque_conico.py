24f568d1d019f54f454f7da37aaec283d7efcce3
igorcleto <igorcleto@outlook.com>
2023-06-08 16:44:44 -0300

    include runge_kutta method

--- tanque_conico.py
+++ tanque_conico.py
@@ -1,13 +1,45 @@
-from math import sqrt
+import numpy as np
 
-C = 0
-h = 0
-R0 = 0
-R1 = 0
-H = 0
-u = 0
+cv = 0.6
+h = 3000.0
+h_t = 0
+R0 = 1
+R1 = 10
+alfa = (R1 - R0)/h
 
-pi = 3.14
-alfa = R1 - R0/H
-qout = C * sqrt(h)
-hponto = [qout/pi * (R0 + alfa * h)^2] + [1/pi * (R0 + alfa * h)^2] * u
+def runge_kutta(qin,duration):
+
+    if duration < 100:
+        return print('escolha um valor de pelo menos 100ms')
+    
+    else:
+
+        h = 3000.0
+        t = 0.0
+        qout = (cv * np.sqrt(h))
+        qout = round(qout.real,2)
+        h = h - qout
+        h = round(h,2)
+
+        # Lista para armazenar os valores calculados
+        t_values = [t]
+        h_values = [h_t]
+
+        while t < duration:
+            # Cálculo do próximo valor usando o método de Runge-Kutta de quarta ordem
+            k1 = h * ((((-1.0 * cv) * np.sqrt(h)) / (np.pi * (R0 + alfa * h)**2.0)) + ((1.0 / (np.pi * (R1 + alfa * h)**2.0)) * qin))
+            k2 = h * ((((-1.0 * cv) * np.sqrt(h)) / (np.pi * (R0 + alfa * (h + 0.5 * k1))**2.0)) + ((1.0 / (np.pi * (R1 + alfa * (h + 0.5 * k1))**2.0)) * qin))
+            k3 = h * ((((-1.0 * cv) * np.sqrt(h)) / (np.pi * (R0 + alfa * (h + 0.5 * k2))**2.0)) + ((1.0 / (np.pi * (R1 + alfa * (h + 0.5 * k2))**2.0)) * qin))
+            k4 = h * ((((-1.0 * cv) * np.sqrt(h)) / (np.pi * (R0 + alfa * (h + k3))**2.0)) + ((1.0 / (np.pi * (R1 + alfa * (h + k3))**2.0)) * qin))
+
+            h = h + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+            t += h
+
+            # Armazenar os valores calculados
+            t_values.append(t)
+            h_values.append(h)
+
+        print(round(h_values[-1],2))
+        return round(h_values[-1],2)
+
+runge_kutta(20.0,100)
